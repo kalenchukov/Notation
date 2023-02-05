ef67218f1067d83706943add220f62d0134c49f4
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2023-02-06 00:28:34 +0300

    Изменена версия требуемого модуля junit до 5.8.2

--- src/test/java/dev/kalenchukov/notation/NotationTest.java
+++ src/test/java/dev/kalenchukov/notation/NotationTest.java
@@ -25,9 +25,9 @@
 package dev.kalenchukov.notation;
 
 import dev.kalenchukov.notation.resources.NotationType;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NotationTest
 {
